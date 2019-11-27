0189d833a60be90547838fe0c2f8b85d057ecab1
Hoả Văn Đoan <hoavandoan@gmail.com>
2019-11-27 20:24:44 +0700

    update

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,31 +1,46 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Alert} from 'react-native';
 import MapView from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
 
 function Map() {
     const [location, setLocation] = useState({
-        latitude: 37.78825,
-        longitude: -122.4324,
+        latitude: null,
+        longitude: null,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
     });
     useEffect(()=>{
-        Geolocation.getCurrentPosition(info => {
-            setLocation({
-                ...location,
-                latitude: info.coords['latitude'],
-                longitute: info.coords['longitude'],
-            });
-            console.log('initialPosition',location)
-        });
+        getLocation()
     },[])
+    function getLocation() {
+        Geolocation.getCurrentPosition(info => {
+                const latitude = JSON.stringify(info.coords.latitude)
+                const longitute = JSON.stringify(info.coords.longitute)
+                setLocation({
+                    ...location,
+                    latitude,
+                    longitute
+                });
+                console.log('initialPosition',location)
+            },
+            error => Alert.alert(error.message),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 });
+        Geolocation.setRNConfiguration({distanceFilter: 5.0})
+    }
     return (
         <View style={styles.container}>
             <MapView
                 style={styles.map}
                 region={location}
+                followsUserLocation={true}
+                showsUserLocation={true}
+                showsMyLocationButton={true}
+                showsCompass={true}
+                toolbarEnabled={true}
+                zoomEnabled={true}
+                rotateEnabled={true}
             >
             </MapView>
         </View>
